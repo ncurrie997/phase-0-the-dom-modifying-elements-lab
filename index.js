9fb4414b84dd4f045a21a35d2a74e170bb407bbe
Nick Currie <gosox997@gmail.com>
2023-04-08 22:59:12 -0400

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainFind = document.getElementById('main');
+mainFind.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.textContent = "Nick is the champion";
